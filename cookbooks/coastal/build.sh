6565ba32e54673f82f255e68d5e7c51c7e09b2f4
taddyb <tkb5476@psu.edu>
2025-09-03 10:56:47 -0400

    tmp: adding tmp files for continuity

---
+++ cookbooks/coastal/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/NGWPC/ngen-forcing.git ./extern/ngen-forcing
+
+git clone https://github.com/NGWPC/schism.git --branch PhilMiller-bmi-interface ./extern/schism
